ee133297473408e43c854acaa76e9b5cfe7f21fa
Aditya06046 <109231656+Aditya06046@users.noreply.github.com>
2022-09-30 09:44:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import ChangeColors from "./arrayobject";
+export default function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColors />
+    </div>
+  );
+}
